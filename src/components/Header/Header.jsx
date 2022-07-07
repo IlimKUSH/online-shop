860c48a188a59d4256461e8a78ebfcecc7b07f15
ilimKUSH <ilimKUSH>
2022-07-07 13:58:58 +0600

    added cart styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import "./header.css";
 
@@ -8,110 +8,44 @@ import Heart from "../../img/heart.svg";
 import Orders from "../../img/orders.svg";
 import Cart from "../../img/cart.svg";
 import Avatar from "../../img/avatar.svg";
-import {useNavigate, useSearchParams} from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Header = () => {
-
   const [searchParam, setSearchParam] = useSearchParams();
 
-  const [search,setSearch] = useState(searchParam.get("q") ? searchParam.get("q") :  "");
-
+  const [search, setSearch] = useState(
+    searchParam.get("q") ? searchParam.get("q") : ""
+  );
 
   const navigate = useNavigate();
 
   useEffect(() => {
     setSearchParam({
-      q:search,
-    })
-  },[search])
+      q: search,
+    });
+  }, [search]);
   return (
     <header className="header">
       <div className="container">
         <div className="header__content">
           <img onClick={() => navigate("/")} src={Logo} alt="logo" />
-          <button className="header__btn">
-            <svg
-              width="29"
-              height="25"
-              viewBox="0 0 28 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <g filter="url(#filter0_d_1812_17810)">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.5 12C4.5 11.7239 4.72386 11.5 5 11.5H23C23.2761 11.5 23.5 11.7239 23.5 12C23.5 12.2761 23.2761 12.5 23 12.5H5C4.72386 12.5 4.5 12.2761 4.5 12Z"
-                  fill="white"
-                />
-              </g>
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4.5 6C4.5 5.72386 4.72386 5.5 5 5.5H23C23.2761 5.5 23.5 5.72386 23.5 6C23.5 6.27614 23.2761 6.5 23 6.5H5C4.72386 6.5 4.5 6.27614 4.5 6Z"
-                fill="white"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4.5 18C4.5 17.7239 4.72386 17.5 5 17.5H23C23.2761 17.5 23.5 17.7239 23.5 18C23.5 18.2761 23.2761 18.5 23 18.5H5C4.72386 18.5 4.5 18.2761 4.5 18Z"
-                fill="white"
-              />
-              <defs>
-                <filter
-                  id="filter0_d_1812_17810"
-                  x="0.5"
-                  y="11.5"
-                  width="27"
-                  height="9"
-                  filterUnits="userSpaceOnUse"
-                  colorInterpolationFilters="sRGB">
-                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="4" />
-                  <feGaussianBlur stdDeviation="2" />
-                  <feComposite in2="hardAlpha" operator="out" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_1812_17810"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_1812_17810"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
-            <p>Каталог</p>
-          </button>
           <div className="header__search">
             <input
-                value={search}
-                onChange={e => setSearch(e.target.value)}
-                type="text"
-                placeholder="Найти товар"
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+              type="text"
+              placeholder="Найти товар"
             />
-            <img src={Search} alt="search"/>
+            <img src={Search} alt="search" />
           </div>
           <div className="header__ftrs">
             <img src={Heart} alt="favorite" />
             <p>Избранное</p>
           </div>
-          <div className="header__ftrs">
+          {/* <div className="header__ftrs">
             <img src={Orders} alt="orders" />
             <p>Заказы</p>
-          </div>
+          </div> */}
           <div onClick={() => navigate("/cart")} className="header__ftrs">
             <img src={Cart} alt="cart" />
             <p>Корзина</p>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,71 +1,100 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
-import {productsContext} from "../../contexts/productsContext";
-import {useSearchParams} from "react-router-dom";
-import {Pagination, Slider} from "@mui/material";
+import { productsContext } from "../../contexts/productsContext";
+import { useSearchParams } from "react-router-dom";
+import { Pagination, Slider } from "@mui/material";
 
 const List = () => {
-    const {getProducts, pages, products, deleteProduct} = useContext(productsContext)
+  const { getProducts, pages, products, deleteProduct } =
+    useContext(productsContext);
 
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const [currentPage, setCurrentPage] = useState(searchParams.get("_page") ? +searchParams.get("_page") : 1)
+  const [currentPage, setCurrentPage] = useState(
+    searchParams.get("_page") ? +searchParams.get("_page") : 1
+  );
 
-    const [price, setPrice] = useState([0, 10000])
-    useEffect(() => {
-        getProducts();
-    }, []);
+  const [price, setPrice] = useState([0, 10000]);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    useEffect(() => {
-        setSearchParams({
-            _page: currentPage,
-            _limit: 8,
-            price_gte: price[0],
-            price_lte: price[1],
-        });
-    }, [currentPage, price]);
+  useEffect(() => {
+    setSearchParams({
+      _page: currentPage,
+      _limit: 8,
+      price_gte: price[0],
+      price_lte: price[1],
+    });
+  }, [currentPage, price]);
 
-    useEffect(() => {
-        getProducts();
-    }, [searchParams]);
+  useEffect(() => {
+    getProducts();
+  }, [searchParams]);
 
-    return (
-        <div>
-
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '500px'
-            }} className="container">
-                <Slider
-                    style={{
-                        color: "#70c05b",
-                        margin: "20px 0",
-                    }}
-                    value={price}
-                    onChange={(e) => {
-                        setPrice(e.target.value)
-                    }}
-                    valueLabelDisplay="auto"
-                    min={0}
-                    max={10000}
-                    step={1}
-                />
-            </div>
-            <div className="products__cards container">
-                {products.map(item => (
-                    <ProductCard
-                        key={item.id}
-                        item={item}
-                        deleteProduct={deleteProduct}
-                    />
-                ))}
-            </div>
-            <Pagination onChange={(event, page) => setCurrentPage(page)}
-                        page={currentPage} variant="outlined" shape="rounded" count={pages}/>
+  return (
+    <div className="container">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "500px",
+          marginTop: "20px",
+          flexDirection: "column",
+        }}
+        className="container">
+        <p
+          style={{
+            fontSize: "18px",
+          }}>
+          По цене:
+        </p>
+        <div
+          style={{
+            width: "600px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            margin: "10px 0 20px 0",
+          }}>
+          <p>0</p>
+          <Slider
+            style={{
+              width: "500px",
+              color: "#70c05b",
+            }}
+            value={price}
+            onChange={e => {
+              setPrice(e.target.value);
+            }}
+            valueLabelDisplay="auto"
+            min={0}
+            max={10000}
+            step={1}
+          />
+          <p>10000</p>
         </div>
-    );
+      </div>
+      <div className="products__cards container">
+        {products.map(item => (
+          <ProductCard
+            key={item.id}
+            item={item}
+            deleteProduct={deleteProduct}
+          />
+        ))}
+      </div>
+      <Pagination
+        style={{ marginTop: "20px" }}
+        onChange={(event, page) => setCurrentPage(page)}
+        page={currentPage}
+        variant="outlined"
+        shape="rounded"
+        count={pages}
+      />
+    </div>
+  );
 };
 
 export default List;
